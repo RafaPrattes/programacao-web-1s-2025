e4901d40ebf85d76a1ec15ab403ddb57784779f6
RafaPrattes <raffaprates92@gmail.com>
2025-03-18 10:23:24 -0300

    fix: correction matriz

--- Atividade02/matrizTransposta.js
+++ Atividade02/matrizTransposta.js
@@ -7,12 +7,12 @@ function transporMatriz(A){
         }
         console.log(linha)
     }
-
+// visita cada elemento das colunas (1,3,5) e depois visita cada elemento das linhas
     console.log("\nTransposta\n")
     for(let j = 0; j < A[0].length; j++){ 
         let linha = ""
-    for(let i = 0; i < A.length; i++){
-        linha += A[i][j] + "\t"
+        for(let i = 0; i < A.length; i++){
+            linha += A[i][j] + "\t"
         }
         console.log(linha)
     }
@@ -23,8 +23,8 @@ function transporMatriz(A){
     5 6
 */
 let matriz = [
-    [1, 2]
-    [3, 4]
+    [1, 2],
+    [3, 4],
     [5, 6]
 ]
 transporMatriz(matriz)
