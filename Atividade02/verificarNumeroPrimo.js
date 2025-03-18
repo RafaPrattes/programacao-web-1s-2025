3e72f2ced3619df1dd84af29a69ba262947c1821
RafaPrattes <raffaprates92@gmail.com>
2025-03-18 10:07:55 -0300

    Correção

--- Atividade02/verificarNumeroPrimo.js
+++ Atividade02/verificarNumeroPrimo.js
@@ -1,11 +1,9 @@
 function verificarNumeroPrimo(n) {
-    if (n <= 1) {
-        return false
-    } else if (n == 2 || n == 3) {
-        return true 
-    } else {
-        
-    }
+
+    
+
+
+} 
 
 
 
