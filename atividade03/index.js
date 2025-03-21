74b9c66d40407432a147d7811775a5d939c16cf9
rafaela <rafaela@ctir.gov.br>
2025-03-21 16:04:21 -0300

    correção

--- atividade03/index.js
+++ atividade03/index.js
@@ -9,19 +9,40 @@ app.get('/ola/:nome', (req, res)=>{
 })
 
 app.get('/somar/:a/:b', (req, res)=>{
-    res.send(`O resultado da soma entre ${a} e ${b} é:\n`)
+//Number.parseFloat= é um método que faz a conversão de string para número
+    const a = Number.parseFloat(req.params.a)
+    const b = Number.parseFloat(req.params.b)
+//foi necessário criar as variaveis para poder usar o req e referenciar
+    const result = a + b
+    res.send(`O resultado da soma entre ${a} e ${b} é: ${result}\n`)
 })
 
 app.get('/subtrair/:a/:b', (req, res)=>{
-    res.send(`O resultado da subtração entre ${a} e ${b} é:\n`)
+    const a = Number.parseFloat(req.params.a)
+    const b = Number.parseFloat(req.params.b)
+
+    const result = a - b
+    res.send(`O resultado da subtração entre ${a} e ${b} é: ${result}\n`)
 })
 
 app.get('/multiplicar/:a/:b', (req, res)=>{
-    res.send(`O resultado da multiplicação entre ${a} e ${b} é:\n`)
+    const a = Number.parseFloat(req.params.a)
+    const b = Number.parseFloat(req.params.b)
+
+    const result = a * b
+    res.send(`O resultado da multiplicação entre ${a} e ${b} é: ${result}\n`)
 })
 
 app.get('/dividir/:a/:b', (req, res)=>{
-    res.send(`O resultado da divisão entre ${a} e ${b} é:\n`)
+    const a = Number.parseFloat(req.params.a)
+    const b = Number.parseFloat(req.params.b)
+
+    if (b === 0) {
+        return res.send("Erro: não é possível dividir por zero.\n")
+    }
+    
+    const result = a / b
+    res.send(`O resultado da divisão entre ${a} e ${b} é: ${result}\n`)
 })
 
 const PORT = 8080
