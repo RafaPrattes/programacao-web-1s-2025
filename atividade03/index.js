38573fc92b683e5a9b003862527e96e9de59398e
RafaPrattes <raffaprates92@gmail.com>
2025-04-01 10:31:30 -0300

    Aprimoramento

--- atividade03/index.js
+++ atividade03/index.js
@@ -1,11 +1,22 @@
+//Carrega o modulo especificado () na variavel calc
 const calc = require('./calculadora')
 const express = require('express')
+//Carrega o módulo express
 
+//cria uma aplicacao do tipo express
 const app = express()
 
 
-app.get('/ola/:nome', (req, res)=>{
-    res.send(`Olá, ${req.params.nome}!`)
+app.get('/', (req, res)=>{
+    let html = '<h1>Calculadora</h1>'
+    html += '<h3>Rotas disponíveis:</h3>'
+    html += '<p>Rotas disponíveis:</p>'
+    html += '<p>/somar/:a/:b <a href="/somar/1/2">testar</a></p>'
+    html += '<p>/subtrair/:a/:b <a href="/subtrair/1/2">testar</a></p>'
+    html += '<p>/multiplicar/:a/:b <a href="/multiplicar/1/2">testar</a></p>'
+    html += '<p>/dividir/:a/:b <a href="/dividir/1/2">testar</a></p>'
+
+    res.send(html)
 })
 
 app.get('/somar/:a/:b', (req, res)=>{
@@ -23,6 +34,8 @@ app.get('/subtrair/:a/:b', (req, res)=>{
 
     const result = a - b
     res.send(`O resultado da subtração entre ${a} e ${b} é: ${result}\n`)
+    //outra forma de concatenar:
+    //res.send(`${a} - ${b} = ${calc.subtrair(a,b)}`)
 })
 
 app.get('/multiplicar/:a/:b', (req, res)=>{
@@ -38,13 +51,14 @@ app.get('/dividir/:a/:b', (req, res)=>{
     const b = Number.parseFloat(req.params.b)
 
     if (b === 0) {
-        return res.send("Erro: não é possível dividir por zero.\n")
+        return res.send("<h1>Erro: não é possível dividir por zero.\n<h1>")
     }
 
     const result = a / b
     res.send(`O resultado da divisão entre ${a} e ${b} é: ${result}\n`)
 })
 
+//Declara a porta 8080
 const PORT = 8080
 
 app.listen(PORT, ()=>{
