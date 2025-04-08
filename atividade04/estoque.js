85c0db073983052c60efbf6138b814e6e8131349
RafaPrattes <rafaprates92@gmail.com>
2025-04-08 10:46:56 -0300

    Correção

--- atividade04/estoque.js
+++ atividade04/estoque.js
@@ -1,8 +1,72 @@
-const dados = [];
+let itens = []
  
-function adicionar (id,nome, qtd){    
-    return id + nome + qtd
+function adicionar (item){    
+
+    let item_valido = true
+
+    if(
+        !is_numerico(item.id)||
+        is_id_cadastrado(item.id)||
+        item.id <= 0 ||
+        !is_numerico(item.qtd)||
+        item.qtd < 0 ||
+        item.nome.length <= 0)
+        {
+       item_valido = false 
+    }
+    if(item.nome.length <= 0){
+        item_valido = false
+    }
+
+    if(item_valido){
+    itens.push(item)//o método push adiciona o que o usuário inserir dentro de itens
+    }
+    return item_valido
 }
+
 function listar(){
-    return produtos
+    return itens
+}
+
+//Percorre o array procurando o objeto com o id solicitado e alterar a qtd de acordo com a solicitação
+function editar (id, qtd){
+//forEach abrevia a escrita do for
+
+if(
+    !is_numerico ||
+    !is_id_cadastrado(id) ||
+    !is_numerico(qtd)
+){
+    item_valido = false
+}
+    itens.forEach(item_cadastrado => {
+        if(item_cadastrado.id == id){
+            item_cadastrado.qtd = qtd
+        }
+    })
+    return true
+}
+
+function is_id_cadastrado(id){
+
+    let id_cadastrado = false
+    itens.forEach(item_cadastrado => {
+        if(item_cadastrado.id == id){
+            item_cadastrado.qtd = qtd
+        }
+    })
+    return id_cadastrado
+}
+
+function is_numerico(n){
+    if(isNaN(n)|| n == null){
+        return false
+    }
+    return true
+}
+//Função para exportar as funções, não se faz necessário exportar o array de itens pois a app não deve acessar diretamente
+module.exports = {
+    adicionar,
+    listar,
+    editar
 }
--- atividade04/index.js
+++ atividade04/index.js
@@ -1,49 +1,67 @@
 //Ao inves de banco de dados, usar um array para armazenar os itens do estoque
+//Evidência de ausência não prova ausência de evidência (não há como provar nada)
+
+/**
+ * 1° - Para ser uma aplicação web deve ser inicializado o Express (framework para Node)
+ * 
+ * 2° - NPM é o gerenciador de pacotes Node, é usado para inicializar os pacotes necessários para a aplicação
+ * 
+ * 3° - NPM init é feito para inicializar a aplicação - 1° passo para transformar em uma app web
+ * 
+ * 4° - Ir até a pasta correta para inicializar o NPM
+ * 
+ * 5° - NPM install express@4.21.2 - Comando para instalar os pacotes  
+ * 
+ * 6° - Importar o express por meio do require
+ * 
+ * 7° - Comando -> node index.js inicia a aplicação e a porta começa a escutar as requisições
+ * 
+ * 8° - 
+ */
 
 
-//Carrega o modulo especificado () na variavel lista
-const lista = require('./estoque')
-const express = require('express')
-//Carrega o módulo express
+const estoque = require('./estoque')//Carrega o modulo especificado () na variavel lista
+const express = require('express')//Carrega o módulo express
+const app = express()//cria uma aplicacao do tipo express
 
-//cria uma aplicacao do tipo express
-const app = express()
-
-
+//Requisição do tipo get com dois parâmetros req(requisição) e res(resposta)
 app.get('/', (req, res)=>{
     let html = '<h1>Estoque</h1>'
+    html += '<h3>Rotas disponíveis:</h3>'
+    html += '<p>/adicionar/:id/:nome/:qtd</p>'
+    html += '<p>/listar</p>'
+    html += '<p>/remover/:id</p>'
+    html += '<p>/editar/:id/:qtd</p>'
     res.send(html)
 })
 
 app.get('/adicionar/:id/:nome/:qtd', (req, res)=>{
 //Number.parseFloat= é um método que faz a conversão de string para número
-    let id = Number.parseFloat(req.params.id)
-    let nome = (req.params.nome)
-    let qtd = (req.params.qtd)
+//Declarar um objeto e colocar os atributos, sempre dentro de chaves
+let item = {
+    id: Number(req.params.id), //Para recuperar o item que está sendo digitado pelo user
+    nome: req.params.nome,
+    qtd: Number(req.params.qtd)//Usar o método Number para converter os valores para numeros
 
-    res.send(`${qtd} produto(s) com o id ${id} e nome ${nome} fora(m) adicionado(s) com sucesso!\n`)
+}
+    res.send(estoque.adicionar(item))
 })
 
 app.get('/listar', (req, res)=>{
-    const produtos = (req.params.produtos)
-    if (produtos.length){
-        console.log('Não há produtos em estoque!')
-    } else {
-        console.log(`${produtos}`)
-    }
-    res.send(`Os produtos em estoque são: ${produtos} \n`)
 
+    res.send(estoque.listar())
 })
 
 app.get('/remover/:id', (req, res)=>{
-    const id = Number.parseFloat(req.params.id)
+    let id = Number(req.params.id)
 
-    res.send(`O produto de id número: ${id} foi removido com sucesso\n`)
+    res.send(`O produto de id número: ${id} foi removido com sucesso!\n`)
 })
 
 app.get('/editar/:id/:qtd', (req, res)=>{
-    const id = Number(req.params.id)
-    const qtd = Number(req.params.qtd)
+    let id = Number(req.params.id)
+    let qtd = Number(req.params.qtd)
+    res.send(estoque.editar(id, qtd))
 
     res.send('Digite o id do produto a ser editado: ')
 })
@@ -51,6 +69,7 @@ app.get('/editar/:id/:qtd', (req, res)=>{
 //Declara a porta 8081
 const PORT = 8081
 
+//A aplicação web fica escutando as requisições em determinada porta
 app.listen(PORT, ()=>{
     console.log(`App rodando na porta correta ${PORT}`)})
 
